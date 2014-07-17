e0af4e4ee4c86ccd427b829733a8f017eb5b3229
Max Lambert <maxlambert@gmail.com>
2014-07-17 18:14:21 -0500

    Updates

--- app/static/base.js
+++ app/static/base.js
@@ -118,3 +118,24 @@ function newPost(){
 }
 
 
+$('img.preview_img').each(function(i, item) {
+    var img_height = $(item).height();
+    var div_height = $(item).parent().height();
+    if(img_height<div_height){
+        //IMAGE IS SHORTER THAN CONTAINER HEIGHT - CENTER IT VERTICALLY
+        var newMargin = (div_height-img_height)/2+'px';
+        $(item).css({'margin-top': newMargin });
+    }else if(img_height>div_height){
+        //IMAGE IS GREATER THAN CONTAINER HEIGHT - REDUCE HEIGHT TO CONTAINER MAX - SET WIDTH TO AUTO  
+        $(item).css({'width': 'auto', 'height': '100%'});
+        //CENTER IT HORIZONTALLY
+        var img_width = $(item).width();
+        var div_width = $(item).parent().width();
+        var newMargin = (div_width-img_width)/2+'px';
+        $(item).css({'margin-left': newMargin});
+    }
+});
+
+
+
+
