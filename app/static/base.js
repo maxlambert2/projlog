6714297d43169fdd41f8746862a46cb7f1fe02b1
Max Lambert <maxlambert@gmail.com>
2014-07-18 16:08:24 -0500

    updates

--- app/static/base.js
+++ app/static/base.js
@@ -136,6 +136,20 @@ $('img.preview_img').each(function(i, item) {
     }
 });
 
+function readURL(input) {
 
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
 
+		reader.onload = function(e) {
+			$('#preview_img').attr('src', e.target.result);
+		}
+
+		reader.readAsDataURL(input.files[0]);
+	}
+}
+
+$('#profile_pic').change(function(){
+	readURL(this);
+}
 
